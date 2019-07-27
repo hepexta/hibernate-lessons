e50f0eebfce0b2256f7a8666689fd75b10fb2418
gunya <hepexta@hotmail.com>
2019-07-27 16:00:20 +0300

    @SequenceGenerator + GenerationType.SEQUENCE (Oracle Example)

--- src/main/java/com/hepexta/lessons/hibernatelessons/data/entities/User.java
+++ src/main/java/com/hepexta/lessons/hibernatelessons/data/entities/User.java
@@ -15,7 +15,8 @@ import java.util.Date;
 @Access(value = AccessType.FIELD)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "USER_ID_SEQ")
     @Column(name = "userId")
     private Long id;
     private String firstName;
